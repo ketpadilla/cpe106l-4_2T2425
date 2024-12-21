d63312755c658f64217ff46170e90811d174baf5
Kristian Padilla <ketpadilla05@gmail.com>
2024-12-21 18:58:02 +0800

    added credits

--- Lab2/encrypt.py
+++ Lab2/encrypt.py
@@ -2,7 +2,9 @@
   LAB 02
   INLAB 01
   
-   Write a script in the file encrypt.py that inputs a line of plaintext and a distance value and outputs an encrypted text using a Caesar cipher. The script should work for any printable characters. (LO: 4.1, 4.2)
+  Write a script in the file encrypt.py that inputs a line of plaintext and a distance value and outputs an encrypted text using a Caesar cipher. The script should work for any printable characters. (LO: 4.1, 4.2)
+
+  Credits: Fundamentals of Python: First Programs Chapter 04
   """
 
 from includes.clear import clearSYS
