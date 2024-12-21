b76bd1364adb3c2b4abb44fc52f8231185a27ab3
Kristian Padilla <ketpadilla05@gmail.com>
2024-12-21 18:54:16 +0800

    changed LAB 01 to LAB 02

--- Lab2/encrypt.py
+++ Lab2/encrypt.py
@@ -1,5 +1,5 @@
 """ 
-  LAB 01
+  LAB 02
   INLAB 01
   
    Write a script in the file encrypt.py that inputs a line of plaintext and a distance value and outputs an encrypted text using a Caesar cipher. The script should work for any printable characters. (LO: 4.1, 4.2)
