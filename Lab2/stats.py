cf3d060325c967560767b29d6d4daa50cd0d37d4
Kristian Padilla <ketpadilla05@gmail.com>
2024-12-21 14:47:54 +0800

    Initial commit for Lab2:
    - copied and revised stats.py and PostLabSolution2.py (renamed as LR2_2.py) into directory
    - copied the includes folder (without stats.py) into directory
    - copied generator.py (renamed as generator_modified.py) into directory

---
+++ Lab2/stats.py
@@ -0,0 +1,28 @@
+""" 
+  EXPERIMENT 02
+  PROGRAMMING PROBLEM 01
+  
+  Statisticians would like to have a set of functions to compute the median and mode of a list of numbers. The median is the number that would appear at the midpoint of a list if it were sorted. The mode is the number that appears most frequently in the list. Define these functions in a module named stats.py. Also include a function named mean, which computes the average of a set of numbers. Each function expects a list of numbers as an argument and returns a single number.
+"""
+
+from includes.clear import clearSYS
+import os
+
+def main():
+  clearSYS()
+  
+  #TODO
+
+def mean():
+  #TODO
+  return
+
+def median():
+  #TODO
+  return
+
+def mode():
+  #TODO
+  return
+
+main()
