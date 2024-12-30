b091bb3929631676382109d74406c0bc83c8c799
Kristian Padilla <ketpadilla05@gmail.com>
2024-12-30 20:06:52 +0800

    finalize encrypt.py

--- Lab2/encrypt.py
+++ Lab2/encrypt.py
@@ -5,7 +5,7 @@
   Write a script in the file encrypt.py that inputs a line of plaintext and a distance value and outputs an encrypted text using a Caesar cipher. The script should work for any printable characters. (LO: 4.1, 4.2)
 
   Credits: Fundamentals of Python: First Programs Chapter 04
-  """
+"""
 
 from includes.clear import clearSYS
 
